0d80f5a497a585feefa2c21d92286f77224638fe
Daniel dos Anjos <daniel.anjos.1989@gmail.com>
2021-02-23 22:34:30 -0300

    #Segundo dia com criação de banco de dados

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from "../models/User";
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
-        console.log(body);
-        return response.send();
+        //desestruturação
+        const { name, email } = request.body;
+
+        const usersRepository = getRepository(User);
+
+        //Validação de email já cadastrado equivalente à:  select * from users where email = "email"
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+        if (userAlreadyExists){
+            return response.status(400).json({
+                error: "User already exists!"
+            })
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+        });
+
+        await usersRepository.save(user);
+
+        return response.json(user);
     }
 }
 
